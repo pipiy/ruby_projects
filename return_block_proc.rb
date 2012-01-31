7fafd202857858f53c1c5a45077386672c2000c7
Artem Slabodnik <artem.slabodnik@gmail.com>
2012-01-31 22:18:14 +0300

    return block_proc

---
+++ return_block_proc.rb
@@ -0,0 +1,12 @@
+#! /usr/bin/env ruby
+
+def return_block
+	yield
+end
+
+def return_proc( &proc )
+	yield
+end
+
+return_block { puts "Got block!" }
+return_proc { puts "Got block, convert to proc!" }
---
+++ scan.rb
@@ -0,0 +1,4 @@
+rub = "And no matter what you’re doing, you can keep on doing it."
+rub.scan(/\w+/)
+end
+puts rub
